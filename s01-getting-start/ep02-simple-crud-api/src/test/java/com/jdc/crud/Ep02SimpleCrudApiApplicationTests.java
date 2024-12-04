4252f247a6df452a37318763b50415428a84be99
Min Lwin <lwin.zawmin@gmail.com>
2024-12-05 02:33:51 +0630

    Share Sinple CRUD Applications

---
+++ s01-getting-start/ep02-simple-crud-api/src/test/java/com/jdc/crud/Ep02SimpleCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdc.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ep02SimpleCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
