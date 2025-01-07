a675c22ccdf379c5f29844e27ff8e01bb31faf8c
Min Lwin <lwin.zawmin@gmail.com>
2025-01-08 01:29:14 +0630

    Share Project

---
+++ s01-getting-start/ep02-simple-crud/ep02-simple-crud-api/src/test/java/com/jdc/react/simple/Ep02SimpleCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdc.react.simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ep02SimpleCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
